7d8f3da1186f42fa12f65e06000357369164f789
willl85 <ngowilfried@gmail.com>
2024-10-12 23:18:53 +0200

    Room details

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <Heading title={'Available Rooms'} />
       {rooms.length > 0 ? (
-        rooms.map((room) => <RoomCard room={room} />)
+        rooms.map((room) => <RoomCard key={room.name} room={room} />)
       ) : (
         <p>No rooms available at the moment</p>
       )}
