24e24d5143c5a29d868f6b27f85a05b53d1853f1
willl85 <ngowilfried@gmail.com>
2024-10-12 21:53:49 +0200

    Header Component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import '@/assets/styles/globals.css';
+import Header from '@/components/Header';
 import { Inter } from 'next/font/google';
 
 // Change the Font to Inter
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           {children}
         </main>
