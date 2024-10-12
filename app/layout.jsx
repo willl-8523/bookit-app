758f4da06f4a264ee90fb493594080696d7ef637
willl85 <ngowilfried@gmail.com>
2024-10-12 22:00:25 +0200

    Footer Component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import '@/assets/styles/globals.css';
+import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import { Inter } from 'next/font/google';
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
