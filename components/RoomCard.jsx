1b36e658f7adc335f80c3888f2819075c1ff7891
willl85 <ngowilfried@gmail.com>
2024-10-13 16:26:51 +0200

    Update responsive of View Room button

--- components/RoomCard.jsx
+++ components/RoomCard.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const RoomCard = ({ room }) => {
   return (
-    <div className="bg-white shadow rounded-lg p-4 mt-4 flex flex-col sm:flex-row justify-between items-start sm:items-center">
+    <div className="bg-white shadow rounded-lg p-4 mt-4 flex flex-col sm:flex-row sm:gap-3 justify-between items-start sm:items-center">
       <div className="flex flex-col sm:flex-row sm:space-x-4">
         <Image
           src={`/images/rooms/${room.image}`}
@@ -19,19 +19,19 @@ const RoomCard = ({ room }) => {
             {room.address}
           </p>
           <p className="text-sm text-gray-600">
-            <span className="font-semibold text-gray-800"> Availability:</span>
+            <span className="font-semibold text-gray-800"> Availability:</span>{' '}
             {room.availability}
           </p>
           <p className="text-sm text-gray-600">
-            <span className="font-semibold text-gray-800"> Price:</span>$
+            <span className="font-semibold text-gray-800"> Price:</span> $
             {room.price_per_hour}/hour
           </p>
         </div>
       </div>
-      <div className="flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0">
+      <div className="w-full flex flex-col sm:flex-row sm:w-28 sm:space-x-2 mt-2 sm:mt-0 lg:w-auto">
         <Link
           href={`/rooms/${room.$id}`}
-          className="bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700"
+          className="bg-blue-500 text-white sm:text-sm px-4 py-2 rounded mb-2 sm:mb-0 w-full lg:text-base  sm:w-28 lg:w-auto text-center hover:bg-blue-700"
         >
           View Room
         </Link>
