763cb9ad8278749ce9970d98093564e57cabda2a
willl85 <ngowilfried@gmail.com>
2024-10-19 22:20:05 +0200

    create appwrite client

---
+++ config/appwrite.js
@@ -0,0 +1,43 @@
+const { Client, Databases, Account, Storage } = require('node-appwrite');
+
+// Admin Client
+const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT)
+    .setKey(process.env.NEXT_APPWRITE_KEY);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+  };
+};
+
+// Session Client
+const createSessionClient = async (session) => {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT);
+
+  if (session) {
+    client.setSession(session);
+  }
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+export { createAdminClient, createSessionClient };
