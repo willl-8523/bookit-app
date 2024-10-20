cd2413df1090ddaf0f27a0b1522e48df7dabddac
willl85 <ngowilfried@gmail.com>
2024-10-20 15:05:16 +0200

    Protecting Bookings Page

--- middleware.js
+++ middleware.js
@@ -2,12 +2,17 @@ import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
   const { pathname } = request.nextUrl;
-
   console.log(`Requested Page: ${pathname}`);
+
+  const isAuthenticated = true;
+
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/login'],
+  matcher: ['/bookings'],
 };
