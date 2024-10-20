74c0fbd5f21f406e34473e4b0b69708e2c3ab1e3
willl85 <ngowilfried@gmail.com>
2024-10-20 17:28:28 +0200

    Logout and Destroy Session

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,26 @@
+'use client';
+
+import destroySession from '@/app/actions/destroySession';
 import logo from '@/assets/images/logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { FaBuilding, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { toast } from 'react-toastify';
 
 const Header = () => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    const { success, error } = await destroySession();
+
+    if (success) {
+      router.push('/login');
+    } else {
+      toast.error(error);
+    }
+  };
+
   return (
     <header className="bg-gray-100">
       <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -60,12 +77,12 @@ const Header = () => {
               <Link href="/rooms/my">
                 <FaBuilding className="inline mr-1" /> My Rooms
               </Link>
-              <Link
-                href="/login"
+              <button
+                onClick={handleLogout}
                 className="mx-3 text-gray-800 hover:text-gray-600"
               >
                 <FaSignOutAlt className="inline mr-1" /> Sign Out
-              </Link>
+              </button>
             </div>
           </div>
         </div>
