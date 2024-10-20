0b6b13a4156056ba5ef3d483ab21dad608c6a7f8
willl85 <ngowilfried@gmail.com>
2024-10-20 16:56:53 +0200

    Generate session and create cookie

--- app/actions/createSession.js
+++ app/actions/createSession.js
@@ -1,5 +1,8 @@
 'use server';
 
+import { createAdminClient } from '@/config/appwrite';
+import { cookies } from 'next/headers';
+
 async function createSession(previousState, formData) {
   const email = formData.get('email');
   const password = formData.get('password');
@@ -10,7 +13,33 @@ async function createSession(previousState, formData) {
     };
   }
 
-  console.log(email, password);
+  // Get account instance
+  const { account } = await createAdminClient();
+
+  try {
+    // Generate session
+    const session = await account.createEmailPasswordSession(email, password);
+
+    // Create cookie
+    // 'appwrite-session' => choose what you want
+    cookies().set('appwrite-session', session.secret, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      expires: new Date(session.expire),
+      path: '/',
+    });
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    console.log('Authentication Error: ', error);
+
+    return {
+      error: 'Invalid Credentials',
+    };
+  }
 }
 
 export default createSession;
