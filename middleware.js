2f6103c1a5824c04b479b79e1619b50a15ca756e
willl85 <ngowilfried@gmail.com>
2024-10-21 12:51:58 +0200

    Finish to protect booking route

--- middleware.js
+++ middleware.js
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server';
+import checkAuth from './app/actions/checkAuth';
 
 export async function middleware(request) {
   const { pathname } = request.nextUrl;
   console.log(`Requested Page: ${pathname}`);
 
-  const isAuthenticated = true;
+  const { isAuthenticated } = await checkAuth();
 
   if (!isAuthenticated) {
     return NextResponse.redirect(new URL('/login', request.url));
